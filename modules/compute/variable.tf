b2d7fb1db1b97981336b75bd313071f56cdb903c
Vinothini-NG <nvinothin21@gmail.com>
2026-07-23 16:04:08 +0530

    add bastion host and provisioner1

--- modules/compute/variable.tf
+++ modules/compute/variable.tf
@@ -2,3 +2,8 @@ variable "compartment_ocid" {}
 variable "tenancy_ocid" {}
 variable "public_subnet_id" {}
 variable "ssh_public_key" {}
+variable "ssh_private_key" {
+  description = "SSH private key"
+  type        = string
+  sensitive   = true
+}
