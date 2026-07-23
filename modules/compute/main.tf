f9e25b7f59e979cb1083fe3d498654b65ec1337e
Vinothini-NG <nvinothin21@gmail.com>
2026-07-23 16:01:12 +0530

    add bastion host and provisioner

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,4 +1,4 @@
-/*CREATE BASTION HOST
+#CREATE BASTION HOST
 data "oci_identity_availability_domains" "ads" {
   compartment_id = var.tenancy_ocid
 }
@@ -39,4 +39,27 @@ resource "oci_core_instance" "bastion_host" {
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
     }
-}*/
+}
+
+resource "null_resource" "bastion_to_private_test" {
+
+  depends_on = [
+    oci_core_instance.bastion_host
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "opc"
+    host        = oci_core_instance.bastion_host.public_ip
+    private_key = var.ssh_private_key
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "hostname",
+      "whoami",
+      "ping -c 1 google.com",
+      "mkdir -p ~/.ssh"
+    ]
+  }
+}
