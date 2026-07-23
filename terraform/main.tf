c83555a194fa4f66cfee7ec2748f2b6755184a58
Vinothini-NG <nvinothin21@gmail.com>
2026-07-23 16:15:34 +0530

    add bastion host and provisioner2

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,4 +10,5 @@ module "compute" {
   tenancy_ocid     = var.tenancy_ocid
   public_subnet_id = module.network.public_subnet_id
   ssh_public_key   = var.ssh_public_key
+  ssh_private_key  = var.ssh_private_key
 }
